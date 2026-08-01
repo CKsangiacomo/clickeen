634564eac7eec154b85b2604884fef6e6f4171f8
P Consavari <pconsavari@gmail.com>
2026-08-01 10:29:24 -0700

    Bridge retirement of San Francisco queue

--- sanfrancisco/src/index.ts
+++ sanfrancisco/src/index.ts
@@ -95,6 +95,10 @@ async function handleModelChat(
 }
 
 export default class SanFranciscoWorker extends WorkerEntrypoint<Env> {
+  async queue(batch: MessageBatch<unknown>): Promise<void> {
+    for (const message of batch.messages) message.ack();
+  }
+
   async fetch(request: Request): Promise<Response> {
     const url = new URL(request.url);
     const requestContext = createSanFranciscoRequestContext(request, this.env);
